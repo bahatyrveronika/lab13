4c2066a5e3713782a9bbb1e9ad681c288dfe36a5
bahatyrveronika <149376695+bahatyrveronika@users.noreply.github.com>
2024-12-02 20:24:13 +0200

    Update BrandfetchReader.java

--- src/main/java/ua/edu/ucu/apps/task3/BrandfetchReader.java
+++ src/main/java/ua/edu/ucu/apps/task3/BrandfetchReader.java
@@ -3,7 +3,6 @@ package ua.edu.ucu.apps.task3;
 public class BrandfetchReader {
 
     public String getLogo(String linkSite) {
-        // Імітація API-виклику до Brandfetch
         return "https://example.com/logo.png";
     }
 }
