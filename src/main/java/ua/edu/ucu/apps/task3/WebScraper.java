204420724073f5f5dc61dcfdb7ef7f2f88c92404
bahatyrveronika <149376695+bahatyrveronika@users.noreply.github.com>
2024-12-02 20:25:25 +0200

    Update WebScraper.java

--- src/main/java/ua/edu/ucu/apps/task3/WebScraper.java
+++ src/main/java/ua/edu/ucu/apps/task3/WebScraper.java
@@ -3,7 +3,6 @@ package ua.edu.ucu.apps.task3;
 public class WebScraper {
 
     public String getAdditionalInfo(String linkSite) {
-        // Імітація веб-скрейпінгу
         return "Additional info about the company from web scraping.";
     }
 }
